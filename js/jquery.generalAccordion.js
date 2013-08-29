31cfecc6186e9b5672ef09f98edf92b252cd6fbf
こうめ <komei1220@gmail.com>
2013-08-29 21:20:10 +0900

    ver 0.0.4

--- js/jquery.generalAccordion.js
+++ js/jquery.generalAccordion.js
@@ -5,12 +5,18 @@
  *--------------------------------------------------------------------------*/
 ;(function($){
 	$.fn.generalAccordion = function(config){
-		//デフォルトオプションの指定
+		// デフォルトオプションの指定
 		var options = $.extend({
-			//デフォルトのパネルが開くスピードの値
+			// デフォルトのパネルが開くスピードの値
 			openSpeed:500,
-			//デフォルトのパネルが閉じるスピードの値
-			closeSpeed:500
+			// デフォルトのパネルが閉じるスピードの値
+			closeSpeed:500,
+			// パネルが開いた後の任意のアクション
+			openAfterAction: function() {
+			},
+			// パネルが開いた後の任意のアクション
+			closeAfterAction: function() {
+			}
 		}, config);
 
 		// 関連付けた各要素(js-generalAccordion)に関数内の処理を実行する
@@ -27,11 +33,11 @@
 				// もし、クリックされた要素hrefに入った要素がdisplay:noneだったら
 				if($btnAttr.css("display") !== "none") {
 					// slideUpを実行
-					$btnAttr.slideUp(options.openSpeed);
+					$btnAttr.slideUp(options.openSpeed, options.closeAfterAction);
 				// display:noneじゃなかったら
 				} else {
-					// slideDownを実行
-					$btnAttr.slideDown(options.closeSpeed);
+					// slideDownを実行後にopenAfterActionで指定したアクションを実行する
+					$btnAttr.slideDown(options.closeSpeed, options.openAfterAction);
 				}
 				// a要素でも大丈夫なように、ブラウザの挙動を止める
 				return false;
