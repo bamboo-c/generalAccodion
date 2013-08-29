6841a26bc2896068d52e51d077f05600f839b134
こうめ <komei1220@gmail.com>
2013-08-29 20:58:09 +0900

    ver 0.0.3

--- js/jquery.generalAccordion.js
+++ js/jquery.generalAccordion.js
@@ -7,10 +7,6 @@
 	$.fn.generalAccordion = function(config){
 		//デフォルトオプションの指定
 		var options = $.extend({
-			//デフォルトのアコーディオンを開閉するボタンのセレクタ
-			generalAccordionBtn:".js-generalAccordionBtn",
-			//デフォルトの開閉するパネルのセレクタ
-			generalAccordionPanel:"js-generalAccordionPanel",
 			//デフォルトのパネルが開くスピードの値
 			openSpeed:500,
 			//デフォルトのパネルが閉じるスピードの値
@@ -20,25 +16,22 @@
 		// 関連付けた各要素(js-generalAccordion)に関数内の処理を実行する
 		this.each(function(){
 			var $self = $(this);
-			var $generalAccordionBtn   = $self.find(options.generalAccordionBtn);
-			var $generalAccordionPanel = $self.find(options.generalAccordionPanel);
+			// ボタンのhrefを変数に入れる
+			var $btnAttr = $($self.attr("href"));
 
 			// パネルを全て隠す
-			$generalAccordionPanel.hide();
+			$btnAttr.hide();
 
 			// ボタンがクリックされた時の処理
-			$generalAccordionBtn.on("click", function(ev){
-				// ボタンのhrefを変数に入れる
-				var btnAttr = $($(this).attr("href"));
-
+			$self.on("click", function(ev){
 				// もし、クリックされた要素hrefに入った要素がdisplay:noneだったら
-				if(btnAttr.css("display") !== "none") {
+				if($btnAttr.css("display") !== "none") {
 					// slideUpを実行
-					btnAttr.slideUp(options.openSpeed);
+					$btnAttr.slideUp(options.openSpeed);
 				// display:noneじゃなかったら
 				} else {
 					// slideDownを実行
-					btnAttr.slideDown(options.closeSpeed);
+					$btnAttr.slideDown(options.closeSpeed);
 				}
 				// a要素でも大丈夫なように、ブラウザの挙動を止める
 				return false;
